524e5e1e2ba988c75c9367746e88feefe244bac8
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2023-12-06 20:24:19 +0530

    complete frontend

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,13 +6,15 @@ import Banner from '../../../public/appBanner.png';
 import Cal from '../../../public/cal.png';
 import Diamond from '../../../public/icons/diamond.svg';
 import CheckIcon from '../../../public/icons/check.svg';
-import { CLIENTS, USERS } from '@/lib/constants';
+import { CLIENTS, PRICING_PLANS, USERS, PRICING_CARDS } from '@/lib/constants';
 import { randomUUID } from 'crypto';
 import { twMerge } from 'tailwind-merge';
 import clsx from 'clsx';
 import CustomCard from '@/components/landing-page/custom-card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { CardContent, CardDescription, CardTitle } from '@/components/ui/card';
+import { Key } from 'lucide-react';
+import { features } from 'process';
 
 const HomePage = () => {
   return (
@@ -205,6 +207,93 @@ const HomePage = () => {
             ))}
           </div>
         ))}
+        </div>
+      </section>
+      <section className='mt-20 px-4 sm:px-6'>
+        <TitleSection
+            title="We Got You Covered"
+            subheading="Experience all the premium features of out platform. Select a plan that suits yout needs and take your productivity to new heights"
+            pill="Pricing"
+        />
+        <div
+          className='flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10'
+        >
+          {PRICING_CARDS.map((card) => (
+           
+            <CustomCard
+            key={card.planType} 
+            className={clsx(
+              'w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative',
+                {
+                  'border-brand-primaryPurple/70':
+                  card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className='text-2xl font-semibold'
+                >
+                  {card.planType=== PRICING_PLANS.proplan && (
+                  <>
+                    <div
+                      className='hidden dark:block w-full blur-[120px] rounded-full h-32 absolute 
+                      bg-primaryPurple/80 
+                      -z-10 
+                      top-0'
+                    />
+                    <Image
+                      src={Diamond}
+                      alt="Pro Plan Icon"
+                      className='absolute top-6 right-6'
+                    />
+                  </>
+                )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className='p-0'>
+                  <span
+                    className='font-normal text-2xl'
+                  >
+                    ${card.price}
+                  </span>
+                  {+card.price> 0 ? (<span className='dark:text-washed-purple-800 ml-1'>
+                    /mo
+                  </span>
+                  ) : (
+                    ''
+                  )} 
+                  <p className='dark:text-washed-purple-800'>{card.description}</p>
+                  <Button variant='btn-primary' className='whitespace-nowrap w-full mt-4'>
+                    {card.planType === PRICING_PLANS.proplan 
+                      ? 'Go Pro'
+                      : 'Get Started'}
+                  </Button>
+                </CardContent>
+              }
+
+              cardFooter={
+                <ul
+                  className='font-normal flex mb-2 flex-col gap-4 '
+                >
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={feature}
+                      className='flex items-center gap-2'
+                    >
+                      <Image
+                        src={CheckIcon}
+                        alt='Check Icon'
+                      />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
+          ))}
         </div>
       </section>
     </>
